97dfa1d8a4d06701d392d5b2f2e4f4cf72d40ced
Ilia Shakitko <ilia.shakitko@accenture.com>
2017-12-07 11:34:58 +0100

    Temporary measure (commenting chown /var/jenkins_home/)

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -22,5 +22,6 @@ echo "2.73.3" > /var/jenkins_home/jenkins.install.UpgradeWizard.state
 
 echo "start JENKINS"
 
-chown -R 1000:1000 /var/jenkins_home
+# Temporary measure, if this folder is big, it holds the container -> holds nginx -> holds whole platform
+# chown -R 1000:1000 /var/jenkins_home
 su jenkins -c /usr/local/bin/jenkins.sh
